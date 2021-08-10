095cee0dbe0db144994f0fc539cefcdc888c55c5
Neezar <rforlyffe@gmaicl.com>
2021-08-10 17:20:31 +0800

    add map

--- src/components/NavOptions.tsx
+++ src/components/NavOptions.tsx
@@ -1,5 +1,12 @@
-import React from "react";
-import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack";
+import React, { FC } from "react";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import { Icon } from "react-native-elements/dist/icons/Icon";
+import tw from "tailwind-react-native-classnames";
+import { RootStackParamList } from "../../App";
+import { useAppSelector } from "../store/hooks";
+import { selectOrigin } from "../store/slices/navSlice";
 
 const data = [
   {
@@ -16,19 +23,36 @@ const data = [
   },
 ];
 
-const NavOptions = () => {
+type IMapScreen = StackNavigationProp<RootStackParamList, "MapScreen">;
+
+const NavOptions: FC = () => {
+  const navigation = useNavigation<IMapScreen | any>();
+  const origin = useAppSelector(selectOrigin);
+
   return (
     <FlatList
       data={data}
+      horizontal
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => (
-        <TouchableOpacity>
-          <View>
+        <TouchableOpacity
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+          onPress={() => navigation.navigate(item.screen)}
+          disabled={!origin}
+        >
+          <View style={tw`${!origin ? "opacity-20" : ""}`}>
             <Image
               style={{ width: 120, height: 120, resizeMode: "contain" }}
               source={{
                 uri: item.image,
               }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
             />
           </View>
         </TouchableOpacity>
